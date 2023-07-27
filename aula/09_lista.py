9b1ed63eda4feab6eb0f55c28cd54d2664bb04ea
Robson Souza/home/robson.jose/.pyenv/versions/3.9.2/bin/python <robson_soujza@hotmail.com>
2023-07-27 19:22:28 -0300

    Listas

--- aula/09_lista.py
+++ aula/09_lista.py
@@ -9,6 +9,10 @@ lista = ['Robson', 1, 5.5, []]
 
 # print(lista)
 # print(type(lista))
+# print(lista[0])
+# print(f'{lista[0]} Souza') #indexação
 
-for i in lista:
-    print(f'Itens da lista: {i}')
+# for i in lista:
+#     print(f'Itens da lista: {i}')
+
+
