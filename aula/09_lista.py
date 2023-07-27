14844067ddb74fe1199ab4e82ffd6ba0426f80e7
Robson Souza/home/robson.jose/.pyenv/versions/3.9.2/bin/python <robson_soujza@hotmail.com>
2023-07-27 19:14:08 -0300

    Listas

---
+++ aula/09_lista.py
@@ -0,0 +1,14 @@
+'''
+Lista
+Temos índice iniciado com o número 0
+A lista é associada aos '[]'
+
+'''
+
+lista = ['Robson', 1, 5.5, []]
+
+# print(lista)
+# print(type(lista))
+
+for i in lista:
+    print(f'Itens da lista: {i}')
