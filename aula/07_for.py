bbcfae57b28cdb27348076b04af6432c71415b05
Robson Souza/home/robson.jose/.pyenv/versions/3.9.2/bin/python <robson_soujza@hotmail.com>
2023-07-30 17:10:36 -0300

    Atualização For e Lista

--- aula/07_for.py
+++ aula/07_for.py
@@ -9,19 +9,28 @@
 # for i,  in range(0, 11, 2):
 #     print(i)
 
-set_ = {1, 2, 3}
-tupla = (1, 2, 3)
-lista = [1, 2, 3]
-dicionario = {'a': 1, 'b': 2}
+# set_ = {1, 2, 3}
+# tupla = (1, 2, 3)
+# lista = [1, 2, 3]
+# dicionario = {'a': 1, 'b': 2}
 
-for elemento in set_:
-    print(f'[SET] Elemento: {elemento}')
+# for elemento in set_:
+#     print(f'[SET] Elemento: {elemento}')
 
-for elemento in tupla:
-    print(f'[Tupla] Elemento: {elemento}')
+# for elemento in tupla:
+#     print(f'[Tupla] Elemento: {elemento}')
 
-for elemento in lista:
-    print(f'[Lista] Elemento: {elemento}')
+# for elemento in lista:
+#     print(f'[Lista] Elemento: {elemento}')
 
-for elemento in dicionario.items():
-    print(f'[Dicionario] Elemento: {elemento}')
+# for elemento in dicionario.items():
+    # print(f'[Dicionario] Elemento: {elemento}')
+
+# nome =  ['Robson', 'Ana', 'Sofia']
+# idade = [39, 40, 7]
+
+# for nomes, idades in zip(nome, idade): #Função Zip permite a interação de duas listas ou mais
+#     print(f'{nomes} tem {idades} anos')
+
+# fatiameto de lista
+
--- aula/09_lista.py
+++ aula/09_lista.py
@@ -2,17 +2,48 @@
 Lista
 Temos índice iniciado com o número 0
 A lista é associada aos '[]'
-
 '''
 
-lista = ['Robson', 1, 5.5, []]
+#INDEXAÇÃO DA LISTA
+# lista = ['Robson', 1, 5.5, []]
+# print(f'{lista[0]} Souza') 
 
+#FATIAMENTO DE LISTA
+# lista = ['a', 'b', 'c', 'd', 'e', 'f']
+# print(lista[0:3:1])
+# print(lista[3:6:1])
+# print(lista[::-1])
+# print(lista[-6:-3:1])
+# print(lista[3::1])
+# print(lista[::2])
+# print(lista[1::2])
+# print(lista[::2] + lista[1::2]) #concatenar lista resultante
+
+#TIPO
 # print(lista)
 # print(type(lista))
-# print(lista[0])
-# print(f'{lista[0]} Souza') #indexação
 
+#PERCORRER A LISTA
+# lista = ['Robson', 1, 5.5, []]
 # for i in lista:
 #     print(f'Itens da lista: {i}')
 
+#MANIPULAÇÃO DE LISTA - METODOS
+lista = ['Robson', 'Ana']
 
+print(lista.append('Sofia')) #acrescenta sem o dado no final da lista
+
+print(lista.extend(['José', 'Iris']))#acrescenta varios dados ou até mesmo, uma outra lista
+
+print(lista.insert(0, 'Valdite'))#acrescenta o dado na lista na posição desejada
+
+print(lista.remove('Valdite'))#remove o primeiro dado passado, no caso de dois dados repetido
+
+print(lista.pop())#remove o último dado da lista, caso não passe nenhuma informação. Passando o indexe, podemos retonar o valor excluído
+nome = lista.pop(2)
+print(f'Nome que estou excluído {nome}')
+print(lista)
+
+# print(lista.clear())#remove todos os valores da lista
+
+print(lista.index('Ana')) #mostra a posição de um determinado valor da lista
