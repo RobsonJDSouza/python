60528976a2c426b8e7d0f9ee87a79aa3dac1c4df
Robson Souza/home/robson.jose/.pyenv/versions/3.9.2/bin/python <robson_soujza@hotmail.com>
2023-07-31 15:13:20 -0300

    Atualização aula de lista

--- aula/09_lista.py
+++ aula/09_lista.py
@@ -29,21 +29,33 @@ A lista é associada aos '[]'
 #     print(f'Itens da lista: {i}')
 
 #MANIPULAÇÃO DE LISTA - METODOS
-lista = ['Robson', 'Ana']
+# lista = ['Robson', 'Ana']
 
-print(lista.append('Sofia')) #acrescenta sem o dado no final da lista
+# print(lista.append('Sofia')) #acrescenta sem o dado no final da lista
 
-print(lista.extend(['José', 'Iris']))#acrescenta varios dados ou até mesmo, uma outra lista
+# print(lista.extend(['José', 'Iris']))#acrescenta varios dados ou até mesmo, uma outra lista
 
-print(lista.insert(0, 'Valdite'))#acrescenta o dado na lista na posição desejada
+# print(lista.insert(0, 'Valdite'))#acrescenta o dado na lista na posição desejada
 
-print(lista.remove('Valdite'))#remove o primeiro dado passado, no caso de dois dados repetido
+# print(lista.remove('Valdite'))#remove o primeiro dado passado, no caso de dois dados repetido
 
-print(lista.pop())#remove o último dado da lista, caso não passe nenhuma informação. Passando o indexe, podemos retonar o valor excluído
-nome = lista.pop(2)
-print(f'Nome que estou excluído {nome}')
-print(lista)
+# print(lista.pop())#remove o último dado da lista, caso não passe nenhuma informação. Passando o indexe, podemos retonar o valor excluído
+# nome = lista.pop(2)
+# print(f'Nome que estou excluído {nome}')
+# print(lista)
 
-# print(lista.clear())#remove todos os valores da lista
+# # print(lista.clear())#remove todos os valores da lista
 
-print(lista.index('Ana')) #mostra a posição de um determinado valor da lista
+# print(lista.index('Ana')) #mostra a posição de um determinado valor da lista
+# print(lista.index('Ana', 0, 4)) #mostra a posição de um determinado valor da lista. Podemos passar o começo e o final do range.
+
+# print(lista.count('Ana'))
+
+# lista.sort() #orderna a lista
+# print(lista)
+
+# lista.sort(reverse=True)#orderna a lista do final para o início
+# print(lista)
+
+# lista.reverse
+# print(lista)
