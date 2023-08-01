241f78da3dc3dcab904ac78cc46b1b5f39736b95
Robson Souza/home/robson.jose/.pyenv/versions/3.9.2/bin/python <robson_soujza@hotmail.com>
2023-07-31 22:45:30 -0300

    Atualização Dicionário

--- aula/10_dicionario.py
+++ aula/10_dicionario.py
@@ -3,48 +3,80 @@ Dicionarios são criados com "{}"
 São mutável
 '''
 
-cadastro = {
-    'id': 1,
-    'nome': 'Robson',
-    'filhos':['Sofia'],
-    'compra':[           # Compras é um dicionário dentro de uma lista
-        {   
-            'id': 19,
-            'produto': 'tenis',
-            'preço': 299.00
-        }
-    ]
+# cadastro = {
+#     'id': 1,
+#     'nome': 'Robson',
+#     'filhos':['Sofia'],
+#     'compra':[           # Compras é um dicionário dentro de uma lista
+#         {   
+#             'id': 19,
+#             'produto': 'tenis',
+#             'preço': 299.00
+#         }
+#     ]
+# }
+
+# # print(cadastro['id'])
+# # print(cadastro['compra'][0])
+# # print(cadastro['compra'][0]['produto'])
+# # print(f'cadastro["compra"][0]["produto"]') #usando F-string com aspas simples, terei que informar o conteúdo do dicionário com as aspas duplas.
+
+# # METODO GET
+# # filhos_get = cadastro.get('filhos') 
+# # print(filhos_get)
+# # altura_get = cadastro.get('altura', None)#com o parametro none, caso passei um informação errada, o get trata o erro trazendo o resultado none.
+# # print(altura_get)
+
+# #ACRESCENTANDO OU MODIFICANDO DADOS
+# # cadastro['nome'] = 'Ana' #aqui eu modifiquei 
+# # cadastro['idade'] = 40   #aqui eu acrescentei
+# # print(cadastro)
+
+# # cadastro.update({'idade': 39, 'nome' : 'Robson'}) #pode atualizar mais de uma informação 
+# # print(cadastro)
+
+# # EXCLUIR
+# del cadastro['nome']
+# print(cadastro)
+
+# cadastro.pop('filhos')
+# print(cadastro)
+
+# cadastro.popitem() #exclui o ultimo item do dicionário
+# print(cadastro)
+
+# cadastro.clear() #exclui todos os itens do dicionário
+# print(cadastro)
+
+# METODOS DO DICIONÁRIO
+familia = {
+    'pai': 'Robson',
+    'mae': 'Ana',
+    'filha': 'Sofia'
 }
 
-# print(cadastro['id'])
-# print(cadastro['compra'][0])
-# print(cadastro['compra'][0]['produto'])
-# print(f'cadastro["compra"][0]["produto"]') #usando F-string com aspas simples, terei que informar o conteúdo do dicionário com as aspas duplas.
+# copia_familia = familia.copy()
+# print(copia_familia)
 
-# METODO GET
-# filhos_get = cadastro.get('filhos') 
-# print(filhos_get)
-# altura_get = cadastro.get('altura', None)#com o parametro none, caso passei um informação errada, o get trata o erro trazendo o resultado none.
-# print(altura_get)
+# item = familia.items() #pega os dois valores
+# print(item)
+# for itens in item:
+#     print(f'Chave:{itens[0]} Valor:{itens[1]}')
 
-#ACRESCENTANDO OU MODIFICANDO DADOS
-# cadastro['nome'] = 'Ana' #aqui eu modifiquei 
-# cadastro['idade'] = 40   #aqui eu acrescentei
-# print(cadastro)
+# chaves = familia.keys()#pega apenas as chaves
+# print(chaves)
+# for i in chaves:
+#     print(i)
 
-# cadastro.update({'idade': 39, 'nome' : 'Robson'}) #pode atualizar mais de uma informação 
-# print(cadastro)
+# valores = familia.values()#pega somente os valores
+# print(valores)
+# for i in valores:
+#     print(i)
 
-# EXCLUIR
-del cadastro['nome']
-print(cadastro)
+# familia.setdefault('sogra', 'Valdite')#insere a chave com o valor passado se a chave não estiver presente n odicionário
+# print(familia)                        #retorna o valor da chave se a hchave já estiver presente no dicionário
 
-cadastro.pop('filhos')
-print(cadastro)
-
-cadastro.popitem() #exclui o ultimo item do dicionário
-print(cadastro)
-
-cadastro.clear() #exclui todos os itens do dicionário
-print(cadastro)
-
+chaves = ['Robson', 'Ana']
+valor = 0
+familia1 = dict.fromkeys(chaves, valor)
+print(familia1)
