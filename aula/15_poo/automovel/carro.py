27c01147dbedee28b539c17d366088efd78d4211
Robson Souza/home/robson.jose/.pyenv/versions/3.9.2/bin/python <robson_soujza@hotmail.com>
2024-03-02 22:18:25 -0300

    POO

--- aula/15_poo/automovel/carro.py
+++ aula/15_poo/automovel/carro.py
@@ -1,4 +1,6 @@
 from main import Automovel
+from time import sleep
+from random import randint
 
 VALOR_PEDAGIO_CARRO = 5.90
 VALOR_KM_RODADO_CARRO = 1.5
@@ -10,8 +12,13 @@ class Carro(Automovel):
 
     def gerar_valor_fatura(self, numero_pedagio, km_rodado ):
         self.valor_fatura = (numero_pedagio * VALOR_PEDAGIO_CARRO) + (VALOR_KM_RODADO_CARRO * km_rodado)
-        print(f'Valor da fatura carro: {self.valor_fatura}')
+        print(f'Valor da fatura carro: R${self.valor_fatura:.2f}')
         
 
 fiesta = Carro("Ford", "fiesta", False)
-fiesta.alugar("Robson")
+fiesta.alugar("Robson") #Chmando o método alugar carro
+
+# simulando tempo que o carro ficou alugado.
+sleep(randint(2,5))
+fiesta.devolver_carro()
+fiesta.gerar_valor_fatura(numero_pedagio= 10, km_rodado=300)
--- aula/15_poo/automovel/main.py
+++ aula/15_poo/automovel/main.py
@@ -11,17 +11,17 @@ class Automovel:
         print(f'Automóvel {self.montadora} {self.modelo} adquirido pela Locadora')
 
 
-    def alugar(self, nome_cliente):
+    def alugar(self, nome_cliente): #alugar é um médoto
         self.alugado = True
         self.nome_cliente = nome_cliente
         print(f'O automóvel {self.montadora} {self.modelo} foi alugado pelo {self.nome_cliente}')
 
-    def devolver_carro(self):
+    def devolver_carro(self): #devolver_carro é um médoto
         self.alugado = False
         print(f'O automóvel {self.montadora} {self.modelo} foi devolvido pelo {self.nome_cliente}')
         self.nome_cliente = ""
 
-    def gerar_valor_fatura(self, numero_pedagio, km_rodado):
+    def gerar_valor_fatura(self, numero_pedagio, km_rodado): #gerar_valor_fatura é um médoto
         raise  NotImplementedError #Exceção. Quem herdar essa classe terar que implementar esse metodo 
     
 
