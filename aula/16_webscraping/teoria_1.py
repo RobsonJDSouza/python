9ba76b874e2ee94dc6bd8f720f1e8d17f7190e26
Robson Souza/home/robson.jose/.pyenv/versions/3.9.2/bin/python <robson_soujza@hotmail.com>
2024-03-19 22:01:19 -0300

    projeto

---
+++ aula/16_webscraping/teoria_1.py
@@ -0,0 +1,94 @@
+'''
+******WEB SCRAPING******
+ 
+É o processo de extrair dados de websites. Isso é feito geralmente através de um programa ou script que acessa páginas da web, analisa o seu conteúdo e extrai as informações desejadas. Essas informações podem ser textos, imagens, vídeos, links, ou qualquer outro tipo de conteúdo disponível na página.
+
+Coleta de dados para análise: 
+Empresas podem usar web scraping para coletar informações sobre preços de produtos, opiniões de clientes, notícias, e outros dados relevantes para análise de mercado.
+
+Monitoramento de preços e concorrência: 
+Varejistas online podem usar web scraping para monitorar os preços de produtos de seus concorrentes e ajustar suas próprias estratégias de preços em resposta.
+
+Agregação de conteúdo: 
+Sites que agregam conteúdo de várias fontes podem usar web scraping para coletar automaticamente artigos, notícias, blogs, etc., de diferentes websites.
+
+Criação de conjuntos de dados: 
+Pesquisadores e cientistas de dados podem usar web scraping para coletar dados para suas análises e estudos.
+
+Automação de tarefas na web: 
+Web scraping pode ser usado para automatizar tarefas repetitivas na web, como preenchimento de formulários, coleta de informações de vários sites, etc.
+
+
+******CRAWLER******
+
+Um crawler, também conhecido como "spider", é um programa de computador que navega pela web de forma automática, seguindo os links entre as páginas para coletar informações. Os crawlers são usados principalmente para indexar conteúdo na web para mecanismos de busca, mas também podem ser utilizados para outras finalidades, como web scraping, verificação de links quebrados, entre outros.
+
+Os crawlers começam geralmente de uma URL inicial, chamada de semente, e em seguida visitam outras páginas, seguindo os links encontrados. Eles podem utilizar diferentes estratégias para decidir quais links seguir e quais páginas visitar, como a análise de relevância do conteúdo, a profundidade da página na hierarquia do site, ou até mesmo políticas específicas definidas pelo criador do crawler.
+
+Os crawlers são uma parte essencial dos motores de busca, pois permitem que eles descubram novas páginas e atualizem regularmente seu índice de resultados de pesquisa. Sem crawlers, os motores de busca não seriam capazes de fornecer resultados relevantes e atualizados aos usuários.
+
+No entanto, é importante observar que nem todos os crawlers são bem-vindos. Alguns podem consumir recursos significativos de um site, como largura de banda e capacidade de processamento, e podem até mesmo violar os termos de serviço do site. Por esse motivo, muitos sites implementam medidas para controlar o acesso de crawlers às suas páginas, como o arquivo robots.txt e os cabeçalhos HTTP específicos para bots.
+
+
+******PARSING******
+
+Parsing é o processo de análise e interpretação de dados em uma determinada estrutura ou formato. Na computação, parsing é frequentemente associado à análise de dados em formato de texto, como linguagens de programação, documentos HTML, XML, JSON, entre outros.
+
+Quando você analisa uma string em uma linguagem de programação para extrair informações específicas ou validar sua estrutura, você está realizando parsing. Por exemplo, ao ler um arquivo JSON em um programa Python e converter seus dados em um objeto Python, você está realizando parsing do JSON.
+
+Existem diferentes técnicas e ferramentas para parsing, dependendo do formato dos dados a serem analisados. Alguns exemplos incluem expressões regulares para parsing de texto simples, analisadores sintáticos (parsers) para linguagens de programação, e bibliotecas específicas para parsing de formatos como HTML, XML e JSON.
+
+Parsing é uma parte fundamental em muitas áreas da computação, incluindo processamento de linguagem natural, compilação de linguagens de programação, interpretação de scripts, análise de dados e muito mais. É um processo essencial para extrair informações significativas e tomar decisões com base nos dados analisados.
+
+
+******SCRAPER******
+
+Um scraper (ou web scraper) é um programa de computador usado para extrair dados de websites de forma automatizada. Os scrapers acessam páginas da web, baixam seu conteúdo e extraem informações específicas conforme necessário. Isso é geralmente feito seguindo padrões específicos de HTML ou utilizando técnicas de parsing para identificar e extrair os dados desejados.
+
+Os scrapers podem ser escritos em várias linguagens de programação, como Python, JavaScript, PHP, entre outras. Eles são frequentemente usados para coletar dados para análise, monitoramento de preços, agregação de conteúdo, e muitas outras finalidades.
+
+No entanto, é importante observar que nem todos os websites permitem ou apoiam o scraping de seus dados. Alguns websites podem ter políticas de uso que proíbem explicitamente o scraping de seus conteúdos. Portanto, ao desenvolver ou usar um scraper, é essencial respeitar os termos de serviço dos websites e garantir que o scraping seja feito de maneira ética e legal.
+
+Existem também serviços e ferramentas disponíveis que oferecem funcionalidades de scraping de forma mais amigável ao usuário, muitas vezes sem a necessidade de escrever código, mas é preciso ter cuidado para não violar os termos de serviço dos websites ao utilizar essas ferramentas.
+
+
+******OFUSCAÇÃO******
+
+Ofuscação, no contexto da computação, refere-se a técnicas utilizadas para tornar o código fonte ou dados mais difíceis de entender ou interpretar. Isso é feito de várias maneiras com o objetivo de dificultar a engenharia reversa, a violação de direitos autorais, ou a proteção de propriedade intelectual. Algumas formas comuns de ofuscação incluem:
+
+Ofuscação de código-fonte: Isso envolve a modificação do código-fonte de um programa de tal forma que seja mais difícil de entender para os humanos. Isso pode incluir renomeação de variáveis e funções para nomes sem sentido, inserção de instruções redundantes ou desnecessárias, reorganização do código para torná-lo menos legível, entre outras técnicas.
+
+Ofuscação de bytecode ou código intermediário: Em linguagens de programação que são compiladas em um bytecode intermediário (como Java, C#, etc.), é possível aplicar técnicas de ofuscação no bytecode gerado. Isso pode envolver a remoção de informações de depuração, a reorganização de instruções, a inserção de instruções falsas, entre outras técnicas.
+
+Ofuscação de dados: Isso envolve a modificação dos dados de tal forma que seja mais difícil para um observador entender seu significado ou propósito. Por exemplo, cifrar ou codificar os dados, ou armazená-los em formatos incomuns.
+
+A ofuscação não fornece segurança absoluta e não impede que alguém determinado decifre o código ou dados ofuscados. No entanto, pode tornar o processo de engenharia reversa mais demorado e difícil, o que pode ser suficiente para dissuadir alguns indivíduos de tentar violar a propriedade intelectual ou descobrir segredos comerciais.
+
+É importante notar que a ofuscação deve ser usada com cuidado, pois pode tornar o código mais difícil de manter e depurar. Também pode ser vista como uma prática controversa em alguns contextos, especialmente quando é usada para obscurecer intencionalmente vulnerabilidades de segurança em um software.
+
+
+******REQUIREMENTS.TXT******
+
+Um arquivo requirements.txt é comumente usado em projetos Python para listar todas as dependências do projeto e suas versões correspondentes. Este arquivo é frequentemente utilizado em conjunto com ferramentas de gerenciamento de pacotes como pip para instalar todas as dependências necessárias para o projeto.
+
+Aqui está um exemplo de como um arquivo requirements.txt pode ser estruturado:
+
+shell
+Copy code
+numpy==1.19.5
+pandas>=1.2.0
+matplotlib<3.4
+scikit-learn~=0.24.1
+tensorflow>=2.4,<3.0
+Neste exemplo:
+
+numpy==1.19.5: O pacote numpy na versão exata 1.19.5 é necessário.
+pandas>=1.2.0: O pacote pandas é necessário, mas qualquer versão igual ou superior a 1.2.0 é aceitável.
+matplotlib<3.4: O pacote matplotlib é necessário, mas qualquer versão abaixo da 3.4 é aceitável.
+scikit-learn~=0.24.1: O pacote scikit-learn é necessário e a versão deve ser próxima à 0.24.1 (não ultrapassando a versão 0.25).
+tensorflow>=2.4,<3.0: O pacote tensorflow é necessário, mas qualquer versão igual ou superior a 2.4 e inferior a 3.0 é aceitável.
+Para instalar as dependências listadas em um arquivo requirements.txt, você pode usar o seguinte comando:
+
+installando as dependências 
+pip install -r requirements.txt
+'''
---
+++ aula/16_webscraping/teoria_2.py
@@ -0,0 +1,32 @@
+'''
+PASSO A PASSO DO WEBSCRAPING
+1º Aquisição da páfina HTML
+2º Processamento dos Dados
+3º geração de informação
+
+
+SITE ALVO
+https://fundamentus.com.br
+
+
+BIBLIOTECAS
+Requests: Realiza requisições http
+BeautifulSoup: Facilita o parsing de páginas HTML
+Tabulate: Formatação de dados no terminal
+'''
+
+'''
+FUNCIONAMENTO DA INTERNET
+Cliente-Servidor
+
+HTML
+Hyper Text Markup Language
+
+CSS
+Cascading Style Sheets" em inglês, que traduzido para o português seria "Folhas de Estilo em Cascata". O CSS é uma linguagem de estilo usada para definir a apresentação de documentos HTML (ou XML).
+
+
+JavaScript 
+É uma linguagem de programação de alto nível, interpretada e orientada a objetos. Originalmente desenvolvida para ser executada no navegador web, ela agora é amplamente utilizada em vários ambientes de desenvolvimento de software, incluindo aplicações web, servidores, dispositivos móveis e até mesmo aplicações de desktop.
+
+'''
