63bdfc6ec3c23a30b74bb27fe66bff7979b4bd4c
Robson Souza/home/robson.jose/.pyenv/versions/3.9.2/bin/python <robson_soujza@hotmail.com>
2024-03-19 22:02:25 -0300

    projeto

--- aula/16_webscraping/1_projeto/main.py
+++ aula/16_webscraping/1_projeto/main.py
@@ -1,32 +1,6 @@
-'''
-PASSO A PASSO DO WEBSCRAPING
-1º Aquisição da páfina HTML
-2º Processamento dos Dados
-3º geração de informação
+import requests
+from bs4 import BeautifulSoup
 
+headers = {'User-Agent': 'Mozilla/5.0'}
+response = requests.get(url='https://fundamentus.com.br/fii_resultado.php', headers=headers)
 
-SITE ALVO
-https://fundamentus.com.br
-
-
-BIBLIOTECAS
-Requests: Realiza requisições http
-BeautifulSoup: Facilita o parsing de páginas HTML
-Tabulate: Formatação de dados no terminal
-'''
-
-'''
-FUNCIONAMENTO DA INTERNET
-Cliente-Servidor
-
-HTML
-Hyper Text Markup Language
-
-CSS
-Cascading Style Sheets" em inglês, que traduzido para o português seria "Folhas de Estilo em Cascata". O CSS é uma linguagem de estilo usada para definir a apresentação de documentos HTML (ou XML).
-
-
-JavaScript 
-É uma linguagem de programação de alto nível, interpretada e orientada a objetos. Originalmente desenvolvida para ser executada no navegador web, ela agora é amplamente utilizada em vários ambientes de desenvolvimento de software, incluindo aplicações web, servidores, dispositivos móveis e até mesmo aplicações de desktop.
-
-'''
