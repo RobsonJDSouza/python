2b667162bdd998bde793cb44e4bfc7c18c52417c
Robson Jose De Souza <robson.jose@olist.com>
2025-01-24 21:57:20 -0300

    Banco de Dados

--- aula/1_string/aula/2_len.py
+++ aula/1_string/aula/2_len.py
@@ -1,9 +1,14 @@
-nome = 'Robson jose de souza'
-email = 'robson@gmail.com'
-print(len(nome))
-print('Robson' in nome)
+#nome = 'Robson jose de souza'
+#email = 'robson@gmail.com'
+#rint(len(nome))
+#print('Robson' in nome)
 
 
-print('Primeiro Caractere ' + nome[0])
-print('Último Caractere ' + nome[-1])
-print('Servidor do email ' + 'gmail' in email)
+#print('Primeiro Caractere ' + nome[0])
+#print('Último Caractere ' + nome[-1])
+#print('Servidor do email ' + 'gmail' in email)
+
+a = "testetestetetestetestetetestexxxxxxxxtestetetestetestetetestete123456"
+
+print(len(a))
+
